6d6e3b56cd3ecc9a938901b040c2433e627e191a
Finn Lam <lamnhanx512@gmail.com>
2026-05-07 15:57:24 +0700

    Add unit tests for JobPostingService using xUnit and InMemory DB

---
+++ JobBoard.Api.Tests/JobPostingServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using JobBoard.Api.Data;
+using JobBoard.Api.DTOs;
+using JobBoard.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobBoard.Api.Tests;
+
+public class JobPostingServiceTests
+{
+    private JobBoardDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<JobBoardDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        return new JobBoardDbContext(options);
+    }
+    [Fact]
+    public async Task CreateJobAsync()
+    {
+        // Arrange
+        var dbContext = GetInMemoryDbContext();
+        var service = new JobPostingService(dbContext);
+        var requestDto = new CreateJobPostingRequest
+        {
+            Title = "Junior .NET Developer",
+            Description = "Great entry level role",
+            Location = "HCM City"
+        };
+        // Act
+        var result = await service.CreateJobAsync(requestDto);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Junior .NET Developer", result.Title);
+        Assert.NotEqual(Guid.Empty, result.Id);
+
+        var jobInDb = await dbContext.JobPostings.FirstOrDefaultAsync();
+        Assert.NotNull(jobInDb);
+        Assert.Equal("HCM City", jobInDb.Location);
+    }
+}
