a7a60bddbb18bfe8763a999780bc511f17e1fdaa
Finn Lam <lamnhanx512@gmail.com>
2026-05-13 13:42:56 +0700

    Update

--- JobBoard.Api.Tests/obj/Debug/net10.0/JobBoard.Api.Tests.AssemblyInfo.cs
+++ JobBoard.Api.Tests/obj/Debug/net10.0/JobBoard.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobBoard.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74e3445502d5e0ac7c815d175301d9e082a8b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201ebf4af58fbee79972510056a6d05505f5f869")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobBoard.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobBoard.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
